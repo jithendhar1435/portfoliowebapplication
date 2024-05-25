eead5d68742baf66c04deac175afa07418f49cb1
Dhanavath Jayanth Naik <jayanthnaikdhavanath@gmail.com>
2024-05-26 02:00:35 +0530

    first commit

---
+++ src/data/user.js
@@ -0,0 +1,84 @@
+const INFO = {
+	main: {
+		title: "Portfolio by Vamshi",
+		name: "Vamshi A.",
+		email: "agnihotramvamsi@gmail.com",
+		logo: "../logo.png",
+	},
+
+	socials: {
+		twitter: "https://twitter.com/",
+		github: "https://github.com/Vamsi-Agnihotram-18",
+		linkedin: "https://www.linkedin.com/in/vamsi-agnihotram-7b19131a8/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+		instagram: "https://instagram.com/",
+		stackoverflow: "https://stackoverflow.com/",
+		facebook: "https://facebook.com/",
+	},
+
+	homepage: {
+		title: "UI/UX Designer & Full-Stack Developer with a Passion for Seamless User Experiences.",
+		description:
+			"I am a backend developer with expertise in Node.js. I have experience in building scalable, secure, and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.",
+	},
+
+	about: {
+		title: "Vamsi Agnihotram, San Jose, CA, shaping tomorrow's design landscape passionately.",
+		description:
+			"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to. If you're interested in any of the projects I've worked on, please feel free to check out the code and suggest any improvements or enhancements you might have in mind. Collaborating with others is a great way to learn and grow, and I'm always open to new ideas and feedback.",
+	},
+
+	articles: {
+		title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
+		description:
+			"Chronological collection of my long-form thoughts on programming, leadership, UX/UI design, and more.",
+	},
+
+	projects: [
+		{
+			title: "Web-app-for Social-issues",
+			description:
+				"The project has 3 portals: user, admin, and authority. User posts or views a social issue, eg: damaged roads. Admin forwards it to the concerned authority, who resolves it eventually.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com/Vamsi-Agnihotram-18/Web-app-for-Social-issues",
+		},
+
+		{
+			title: "https://github.com/Vamsi-Agnihotram-18/urbanguide",
+			description:
+				"UrbanGuide: Prototype ( PHP Project) for your ultimate urban companion. Discover local businesses, eateries, and entertainment venues with trusted reviews. Explore hidden gems, stay updated on urban trends, and connect with like-minded explorers.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
+			linkText: "View Project",
+			link: "https://github.com/Vamsi-Agnihotram-18/urbanguide",
+		},
+
+		{
+			title: "Cineflix---Full-stack-Application",
+			description:
+				"Developed by Agile & Scrum ",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+			linkText: "View Project",
+			link: "https://github.com/Vamsi-Agnihotram-18/Cineflix---Full-stack-Application",
+		},
+
+		{
+			title: "Twitter-Data-Analysis",
+			description:
+				"Hadoop framework to analyze unstructured data that we obtain from Twitter and perform sentiment and trend analysis using Hive on MapReduce and Spark on keyword “COVID19”",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com/Vamsi-Agnihotram-18/Twitter-Analysis-Hadoop-Spark-Hive",
+		},
+
+		{
+			title: "Simple Cloud Infrastructure Manager (SCIM)",
+			description:
+				"SCIM is a lightweight tool designed to streamline the management of cloud resources. Developed with a focus on automation and efficient configuration management, SCIM provides a robust solution for deploying, configuring, and maintaining servers and storage across cloud environments",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com/Vamsi-Agnihotram-18/SCIM-Terraform-Ansible-AWS-Java",
+		},
+	],
+};
+
+export default INFO;
